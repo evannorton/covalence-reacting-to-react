26fdae7893df17e5350a04601820923c4a29d97b
Evan Norton <evanan39@yahoo.com>
2018-01-24 09:51:44 -0600

    objective 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 
+/*
 const App = (props) => {
     return (
         <h1>{props.text}</h1>
     );
+}
+*/
+
+class App extends Component {
+    render() {
+        return (
+            <h1>{this.props.text}</h1>
+        );
+    }
 }
 
 export default App;
